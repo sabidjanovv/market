4d0d82c4af0158cff8ada18099587a441bd348a5
sabidjanovv <sabidjanov@gmail.com>
2024-11-29 12:00:42 +0500

    Finally

--- src/components/Carausel.jsx
+++ src/components/Carausel.jsx
@@ -3,6 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import image1 from "../assets/image1.jpg";
 import image2 from "../assets/image2.jpg";
 import image3 from "../assets/image3.jpg";
+import image4 from "../assets/image4.jpg";
+
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -46,6 +48,16 @@ const Carausel = () => {
             className="w-full h-full object-cover absolute top-0 left-0"
             src={image3}
             alt="Banner 3"
+          />
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div className="h-[400px] md:h-[600px] lg:h-[800px] border relative">
+          <img
+            className="w-full h-full object-cover absolute top-0 left-0"
+            src={image4}
+            alt="Banner 4"
           />
         </div>
       </SwiperSlide>
